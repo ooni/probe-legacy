a5ab366cb28955fb48dedaac106dbdf6e0a09dda
Arturo Filastò <art@fuffa.org>
2014-09-18 22:03:11 +0200

    Implement unittest for http client by mocking the transport.

--- ooni/tests/test_network.py
+++ ooni/tests/test_network.py
@@ -1,86 +1,99 @@
-from twisted.internet import defer, reactor
+from twisted.internet import defer
 from twisted.trial import unittest
 
+from twisted.test import proto_helpers
 from twisted.web.test.test_agent import FakeReactorAndConnectMixin
+from twisted.web.client import Agent
+from twisted.web._newclient import Response
 
-from ooni.network import http, randomFreePort
+from ooni.network import http
 
 import twisted.internet.base
 twisted.internet.base.DelayedCall.debug = True
 
-from ooni.tests import is_internet_connected
-from ooni.tests.network_helpers import returnHeadersSite
-
-class BaseNetworkTest(unittest.TestCase):
+class TestHTTPAgentWithFakeTransport(unittest.TestCase, FakeReactorAndConnectMixin):
     def setUp(self):
-        self.portNumber = randomFreePort()
-        self.httpServer = reactor.listenTCP(self.portNumber, returnHeadersSite)
-        self.baseUrl = 'http://127.0.0.1:%d' % self.portNumber
+        self.reactor = proto_helpers.MemoryReactorClock()
+        self.agent = Agent(self.reactor)
+        self.agent._getEndpoint = lambda *args: self
 
-    def tearDown(self):
-        return self.httpServer.stopListening()
+    def test_get_request_header_list(self):
+        headers = [('SoMe-Header', ['SomeValue']),
+                   ('HosT', ['example.com'])]
+        request = http.Request(self.reactor, self.agent)
+        request.get('http://127.0.0.1/',
+                    headers=headers)
+        request = self.protocol.requests[0][0]
+        self.assertEqual(list(request.headers.getAllRawHeaders()),
+                         headers)
+        self.assertEqual(request.method, "GET")
 
-class TestNetworkOffline(BaseNetworkTest):
-    @defer.inlineCallbacks
-    def test_get_request(self):
-        request = http.Request()
-        response = yield request.get(self.baseUrl + '/redirect/2',
-                                     headers={'Some-Header': ['Value']},
-                                     follow_redirects=True)
-        print response.headers
-        print response.body
-        print response.code
-        for r in response.responseChain():
-            print r.pprint()
-
-class TestNetworkViaInternetHTTP(unittest.TestCase, FakeReactorAndConnectMixin):
-
-    def setUp(self):
-        if not is_internet_connected():
-            self.skipTest(
-                "You must be connected to the internet to run this test"
-            )
-        self.request = http.Request()
-
-    @defer.inlineCallbacks
-    def test_get_request_google(self):
-        request = http.Request()
-        response = yield request.get('http://google.com/',
-                                     headers={'Some-Header': ['Value']},
-                                     follow_redirects=True)
-        print response.headers
-        print response.body
-        print response.code
-        for r in response.responseChain():
-            print r.pprint()
+    def test_get_request_header_dict(self):
+        headers = {
+            'SoMe-Header': ['SomeValue'],
+            'HosT': ['example.com']
+        }
+        request = http.Request(self.reactor, self.agent)
+        request.get('http://127.0.0.1/',
+                    headers=headers)
+        request = self.protocol.requests[0][0]
+        for name, value in headers.items():
+            self.assertEqual(request.headers.getRawHeaders(name), value)
+        self.assertEqual(request.method, "GET")
 
     @defer.inlineCallbacks
-    def test_get_request(self):
-        request = http.Request()
-        response = yield request.get('http://httpbin.org/redirect/2',
-                                     headers={'Some-Header': ['Value']},
-                                     follow_redirects=True)
-        print response.headers
-        print response.body
-        print response.code
-        for r in response.responseChain():
-            print r.pprint()
+    def test_get_with_body(self):
+        headers = {
+            'SoMe-Header': ['SomeValue'],
+            'HosT': ['example.com']
+        }
+        request = http.Request(self.reactor, self.agent)
+        deferred = request.get('http://127.0.0.1/',
+                               headers=headers)
+        req, res = self.protocol.requests.pop()
+
+        headers = http.TrueOrderedHeaders({
+            'Content-length': [4]
+        })
+        tr = proto_helpers.StringTransport()
+        response = Response(('HTTP', 1, 1), 200, 'OK', headers, tr)
+        res.callback(response)
+        response._bodyDataReceived("SPAM")
+        response._bodyDataFinished()
+        result = yield deferred
+        self.assertEqual(result.body, "SPAM")
 
     @defer.inlineCallbacks
-    def test_get_request_proxy(self):
-        request = http.Request()
-        response = yield request.get('http://httpbin.org/redirect/2',
-                                     headers={'Some-Header': ['Value']},
-                                     follow_redirects=True,
-                                     proxy=('127.0.0.1', 9050))
-        print response.headers
-        print response.body
-        print response.code
-        for r in response.responseChain():
-            print r.pprint()
+    def test_get_and_redirect(self):
+        headers = {
+            'SoMe-Header': ['SomeValue'],
+            'HosT': ['example.com']
+        }
+        request = http.Request(self.reactor, self.agent)
+        deferred = request.get('http://127.0.0.1/',
+                               headers=headers,
+                               follow_redirects=True)
+        req, res = self.protocol.requests.pop()
 
-    def test_get_headers(self):
-        pass
+        headers = http.TrueOrderedHeaders({
+            'Location': ['/ham']
+        })
+        tr = proto_helpers.StringTransport()
+        response = Response(('HTTP', 1, 1), 301, 'Moved Permanently', headers, tr)
+        res.callback(response)
+        response._bodyDataFinished()
 
-    def test_get_body(self):
-        pass
+        headers = http.TrueOrderedHeaders({
+            'Good-Job': ['Jane']
+        })
+        req, res = self.protocol.requests.pop()
+        self.assertEqual(req.uri, '/ham')
+        response = Response(('HTTP', 1, 1), 200, 'OK', headers, tr)
+        res.callback(response)
+        response._bodyDataFinished()
+
+        result = yield deferred
+        responses = list(result.responseChain())
+        self.assertEqual(len(responses), 2)
+        self.assertEqual(responses[0].headers.getRawHeaders('Good-Job'),
+                         ['Jane'])
